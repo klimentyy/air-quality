cb628c2ba0192461b6e77c54e76580391d1c9fe2
Klim Shmelev <klimenty.shmelev@gmail.com>
2026-07-10 16:30:05 +0200

    refactor AppConfig to remove hardcoded BUCKET_NAME [no ci]

--- src/air_quality/config.py
+++ src/air_quality/config.py
@@ -6,13 +6,11 @@ class AppConfig:
     ENV: str = os.environ.get("ENV", "dev").lower()
     IS_LAMBDA: bool = os.environ.get("AWS_LAMBDA_FUNCTION") is not None
     ALLOW_CLOUD_WRITE: bool = IS_LAMBDA or (
-        os.environ.get("ALLLOW _LOCAL_CLOUD_WRITE", "false").lower() == "true"
+        os.environ.get("ALLOW_LOCAL_CLOUD_WRITE", "false").lower() == "true"
     )
 
     # Infrastructure storage paths
-    BUCKET_NAME: str = os.environ.get(
-        "DATA_HEALTH_BUCKET_NAME", f"prague-air-quality-project-data-lake-{ENV}"
-    )
+    BUCKET_NAME: str = os.environ.get("DATA_HEALTH_BUCKET_NAME", "")
     S3_TARGET_KEY: str = "extracted/air_quality_latest.parquet"
     LOCAL_TARGET_PATH: str = "air_quality_latest.parquet"
 
@@ -22,8 +20,12 @@ class AppConfig:
     GOLEMIO_API_TOKEN: str = os.environ.get("GOLEMIO_API_TOKEN", "")
 
     @classmethod
-    def validate(clsself) -> None:
-        if not clsself.GOLEMIO_API_TOKEN:
+    def validate(cls) -> None:
+        if not cls.GOLEMIO_API_TOKEN:
             raise ValueError(
                 "CRITICAL: GOLEMIO_API_TOKEN is missing in environment variables."
+            )
+        if cls.ALLOW_CLOUD_WRITE or cls.ENV != "dev" and not cls.BUCKET_NAME:
+            raise ValueError(
+                "CRITICAL: BUCKET_NAME is missing in environment variables."
             )
